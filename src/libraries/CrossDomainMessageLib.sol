82c1a1186e870b8cb7d9b2572a19cb3d88054078
James Kim <jakim929@gmail.com>
2025-02-11 18:06:38 -0800

    feat: add CrossDomainMessageLib.sol

---
+++ src/libraries/CrossDomainMessageLib.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {PredeployAddresses} from "./PredeployAddresses.sol";
+import {IL2ToL2CrossDomainMessenger} from "../interfaces/IL2ToL2CrossDomainMessenger.sol";
+
+library CrossDomainMessageLib {
+    error DependentMessageNotSuccessful(bytes32 msgHash);
+    error CallerNotL2ToL2CrossDomainMessenger();
+    error InvalidCrossDomainSender();
+
+    function requireMessageSuccess(bytes32 msgHash) internal view {
+        if (
+            !IL2ToL2CrossDomainMessenger(PredeployAddresses.L2_TO_L2_CROSS_DOMAIN_MESSENGER).successfulMessages(msgHash)
+        ) {
+            revert DependentMessageNotSuccessful(msgHash);
+        }
+    }
+
+    function requireCrossDomainCallback() internal view {
+        if (msg.sender != address(PredeployAddresses.L2_TO_L2_CROSS_DOMAIN_MESSENGER)) {
+            revert CallerNotL2ToL2CrossDomainMessenger();
+        }
+        if (
+            IL2ToL2CrossDomainMessenger(PredeployAddresses.L2_TO_L2_CROSS_DOMAIN_MESSENGER).crossDomainMessageSender()
+                != address(this)
+        ) revert InvalidCrossDomainSender();
+    }
+}
